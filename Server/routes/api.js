ced0e5b2886200439e02748f01a330135b831ee3
Mendi Toledano <menditoledano@gmail.com>
2018-01-17 23:15:38 +0200

    remove api request

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -12,17 +12,17 @@ router.get('/', (req, res) => {
 
 
 
-// Get all posts
-router.get('/posts', (req, res) => {
-  // Get posts from the mock api
-  // This should ideally be replaced with a service that connects to MongoDB
-  axios.get(`${API}/posts`)
-    .then(posts => {
-      res.status(200).json(posts.data);
-    })
-    .catch(error => {
-      res.status(500).send(error)
-    });
-});
+// // Get all posts
+// router.get('/posts', (req, res) => {
+//   // Get posts from the mock api
+//   // This should ideally be replaced with a service that connects to MongoDB
+//   axios.get(`${API}/posts`)
+//     .then(posts => {
+//       res.status(200).json(posts.data);
+//     })
+//     .catch(error => {
+//       res.status(500).send(error)
+//     });
+// });
 
 module.exports = router;
