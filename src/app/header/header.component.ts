ff20192e9f5e7e77649eaa706a69bcb93cf2d649
Mendi Toledano <menditoledano@gmail.com>
2018-04-17 23:09:50 +0300

    update ceriers and lot of css changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as bootstrap from 'bootstrap';
+// import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-header',
@@ -13,3 +16,4 @@ export class HeaderComponent implements OnInit {
   }
 
 }
+
