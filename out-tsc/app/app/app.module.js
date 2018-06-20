8cef4455f5fc8dcb2ee976cc07002f3847cffec5
Mendi Toledano <menditoledano@gmail.com>
2018-06-20 19:24:47 +0300

    Add Whatsapp button

---
+++ out-tsc/app/app/app.module.js
@@ -0,0 +1,52 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+// Import the Http Module and our Data Service
+import { HttpModule } from '@angular/http';
+import { DataService } from './data.service';
+import { PostsService } from './posts.service';
+import { HeaderComponent } from './header/header.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PostsComponent } from './pages/posts/posts.component';
+import { RouterModule } from "@angular/router";
+import { FooterComponent } from './footer/footer.component';
+var routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'home', component: HomePageComponent },
+    { path: 'about', component: AboutPageComponent },
+    { path: 'posts', component: PostsComponent },
+    { path: '**', component: HomePageComponent }
+];
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        NgModule({
+            declarations: [
+                AppComponent,
+                HeaderComponent,
+                AboutPageComponent,
+                HomePageComponent,
+                FooterComponent,
+                PostsComponent
+            ],
+            imports: [
+                BrowserModule,
+                HttpModule,
+                RouterModule.forRoot(routes, { useHash: true })
+            ],
+            providers: [DataService, PostsService],
+            bootstrap: [AppComponent]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+export { AppModule };
+//# sourceMappingURL=app.module.js.map
---
+++ out-tsc/app/app/data.service.js
@@ -0,0 +1,34 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+var DataService = /** @class */ (function () {
+    function DataService(_http) {
+        this._http = _http;
+    }
+    DataService.prototype.getUsers = function () {
+        var _this = this;
+        return this._http.get("/api/users")
+            .map(function (result) { return _this.result = result.json().data; });
+    };
+    DataService.prototype.getimage = function () {
+        var _this = this;
+        return this._http.get("/api/image")
+            .map(function (result) { return _this.result = result.json().data; });
+    };
+    DataService = __decorate([
+        Injectable(),
+        __metadata("design:paramtypes", [Http])
+    ], DataService);
+    return DataService;
+}());
+export { DataService };
+//# sourceMappingURL=data.service.js.map
