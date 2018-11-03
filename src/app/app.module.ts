7565d67b48198aaff0e0dde8a912ad92d407d0e1
Mendi Toledano <menditoledano@gmail.com>
2018-11-03 21:20:23 +0200

    clean project v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { MatButtonModule, MatCheckboxModule } from "@angular/material";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
 // Import the Http Module and our Data Service
-import { HttpModule } from '@angular/http';
-import { DataService } from './data.service';
-import { PostsService } from './posts.service';
-import { HeaderComponent } from './header/header.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { PostsComponent } from './pages/posts/posts.component';
-import { RegisterComponent } from './pages/register/register.component';
+import { HttpModule } from "@angular/http";
+// import { DataService } from './data.service';
+// import { PostsService } from './posts.service';
+import { HeaderComponent } from "./header/header.component";
+import { AboutPageComponent } from "./pages/about-page/about-page.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+// import { PostsComponent } from "./pages/posts/posts.component";
+import { RegisterComponent } from "./pages/register/register.component";
 
-import {Routes, RouterModule} from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
-import { StoreComponent } from './pages/store/store.component';
-
-
+import { Routes, RouterModule } from "@angular/router";
+import { FooterComponent } from "./footer/footer.component";
+import { StoreComponent } from "./pages/store/store.component";
 
 const routes: Routes = [
-  { path: 'home', component: HomePageComponent },
-  { path: 'about', component: AboutPageComponent },
-  { path: 'posts', component: PostsComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'store', component: StoreComponent },
-  {path: '**', component: HomePageComponent}
- ];
+  { path: "home", component: HomePageComponent },
+  { path: "about", component: AboutPageComponent },
+  // { path: "posts", component: PostsComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "store", component: StoreComponent },
+  { path: "**", component: HomePageComponent }
+];
 
 @NgModule({
   declarations: [
@@ -36,19 +34,19 @@ const routes: Routes = [
     AboutPageComponent,
     HomePageComponent,
     FooterComponent,
-    PostsComponent,
+    // PostsComponent,
     RegisterComponent,
     StoreComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule ,
+    HttpModule,
     MatButtonModule,
     MatCheckboxModule,
-    MatProgressSpinnerModule,           // <-Add HttpModule
-    RouterModule.forRoot(routes, {useHash: true})
+    MatProgressSpinnerModule, // <-Add HttpModule
+    RouterModule.forRoot(routes, { useHash: true })
   ],
-  providers: [DataService, PostsService], // <-Add DataService
+  providers: [], // <-Add DataService
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
-import "rxjs/add/operator/map";
+// import { Injectable } from "@angular/core";
+// import { Http } from "@angular/http";
+// import "rxjs/add/operator/map";
 
-@Injectable()
-export class PostsService {
-  constructor(private http: Http) {}
+// @Injectable()
+// export class PostsService {
+//   constructor(private http: Http) {}
 
-  // Get all posts from the API
-  getAllPosts() {
-    return this.http.get("/api/posts").map(res => res.json());
-  }
-}
+//   // Get all posts from the API
+//   getAllPosts() {
+//     return this.http.get("/api/posts").map(res => res.json());
+//   }
+// }
