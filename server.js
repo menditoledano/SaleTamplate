8f6506c4a62eb6971a696877dc1dbff6f242f9f9
Mendi Toledano <menditoledano@gmail.com>
2019-06-26 22:40:15 +0300

    /

--- server.js
+++ server.js
@@ -42,4 +42,5 @@ const server = http.createServer(app);
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+// server.listen(port, () => console.log(`API running on localhost:${port}`));
+app.listen(port, () => console.log(`API running on localhost:${port}`));
